da8f32374909fa1b608e174ef32fccf8b182786a
Arjun Singh Kandari <116184991+Kandariarjun07@users.noreply.github.com>
2024-07-29 16:39:53 +0530

    Time: 546 ms (36.1%), Space: 218.3 MB (21.3%) - LeetHub

--- 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
-    using int2 = pair<int, int>;
-
-    inline static int wtime(int step, int time, int change) {
-        int ans=0;
-        for(int i=0; i < step; i++) {
-            int gr=ans/change;
-            if (gr&1)  // If it's a red light
-                ans=(gr+ 1)*change;
-            ans+=time;
-        }
-        return ans;
-    }
-
-    static int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> adj(n + 1);
-        for (auto& e : edges) {
+#define P pair<int, int>
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        unordered_map<int, vector<int>> adj(n + 1);
+        for (auto e : edges) {
             int v = e[0], w = e[1];
             adj[v].push_back(w);
             adj[w].push_back(v);
         }
-
-        vector<int> dist(n+1, INT_MAX), dist2(n+1, INT_MAX);
-        queue<int2> q;  // {node, distance}
-        q.emplace(1, 0);
-        dist[1]=0;
-
-        while (!q.empty()) {
-            auto [x, d]=q.front();
-            q.pop();
-            for (int y : adj[x]) {
-                int newD=d+1;
-                if (newD < dist[y]) {
-                    dist2[y]=dist[y];
-                    dist[y]=newD;
-                    q.emplace(y, newD);
-                } 
-                else if (newD>dist[y] && newD<dist2[y]) {
-                    dist2[y]=newD;
-                    q.emplace(y, newD);
+        vector<int> d1(n + 1, INT_MAX), d2(n + 1, INT_MAX);
+        queue<P> que;
+        que.push({1, 1});
+        d1[1] = 0;
+        while (!que.empty()) {
+            auto [node, freq] = que.front();
+            que.pop();
+            int timePassed = (freq == 1) ? d1[node] : d2[node];
+            int div = timePassed / change;
+            if (div % 2 == 1) {
+                timePassed = (div + 1) * change;
+            }
+            if (node == n && d2[n] != INT_MAX)
+                return d2[n];
+            for (auto& nbr : adj[node]) {
+                if (d1[nbr] == INT_MAX) {
+                    d1[nbr] = timePassed + time;
+                    que.push({nbr, 1});
+                } else if (d2[nbr] == INT_MAX && timePassed + time != d1[nbr]) {
+                    d2[nbr] = timePassed + time;
+                    que.push({nbr, 2});
                 }
             }
         }
-
-        int steps=dist2[n];
-        if (steps == INT_MAX) return -1;
-        return wtime(steps, time, change);
+        return -1;
     }
 };
