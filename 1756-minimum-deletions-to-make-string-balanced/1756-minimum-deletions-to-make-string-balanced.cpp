622b1ff1edb47dbf66365587bd76f3a8c8c17d9c
Arjun Singh Kandari <116184991+Kandariarjun07@users.noreply.github.com>
2024-07-31 10:22:04 +0530

    Time: 91 ms (59.94%), Space: 23.5 MB (76.84%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int b_count = 0;
-        int min_deletions = 0;
-
+        int a_count = 0, b_count = 0;
+        int res = INT_MAX;
+        for (auto c : s)
+            if (c == 'a')
+                a_count++;
         for (char c : s) {
-            c == 'b' ? b_count++ : min_deletions = min(min_deletions + 1, b_count);
+            if (c == 'a')
+                a_count--;
+            res = min(res, a_count + b_count);
+            if (c == 'b')
+                b_count++;
         }
-
-        return min_deletions;
+        return res;
     }
 };
